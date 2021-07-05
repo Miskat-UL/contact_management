284f8586c0ffd64772b7b6491b665599ea8d6c24
Miskat-UL <53708135+miShowmik@users.noreply.github.com>
2021-07-06 00:36:00 +0600

    Up

---
+++ app.py
@@ -0,0 +1,14 @@
+ # contact management system
+
+
+def menu():
+
+    user_choice = input("""
+        Enter C - For create contact
+        Enter S - For show all contact
+        Enter D - For delete any contact
+        Enter M - For add another(extra) number to the same person
+        Enter S - For search a contact
+        
+    """)
+menu()
